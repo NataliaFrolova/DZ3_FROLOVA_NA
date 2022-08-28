8408c176bf6865ffd5b27e5a0539aa51d681e88a
Natalia Frolova <n.smirnova76@mail.ru>
2022-08-28 15:36:57 +0300

    task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+string numberA = Console.ReadLine();
+
+int numberB = Convert.ToInt32(numberA);
+
+numberA.Length.ToString();
+
+if (numberB > 9999 && numberB < 100000)
+{
+    Console.Write("Это полиндром? ");
+
+    if (numberA[0] == numberA[4] && numberA[1] == numberA[3])
+    {
+        Console.WriteLine("да ");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+else Console.WriteLine("Bad number");
+
